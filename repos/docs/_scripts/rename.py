b96d5516a82b61b543526c07485e19362ffb1e16
Me <32935713+jayantmathur@users.noreply.github.com>
2023-05-31 17:44:47 -0400

    repo: misc update

--- repos/docs/_scripts/rename.py
+++ repos/docs/_scripts/rename.py
@@ -1,21 +1,17 @@
 import time
 import os
+import shutil
+
 
 # Get the path of the file
 filepath = "./output/index.pdf"
 
 # Get the date of the file
-timestamp = time.strftime(
-    "%m%d",
-    time.strptime(
-        time.ctime(
-            os.path.getctime(filepath)
-        )
-    )
-)
+timestamp = time.strftime("%m%d", time.strptime(time.ctime(os.path.getctime(filepath))))
 
-# Rename the file
-os.rename(
-    filepath, 
-    os.path.split(filepath)[0] + "/ver" + timestamp + os.path.splitext(filepath)[1]
+# Copy the file
+
+shutil.copyfile(
+    filepath,
+    os.path.split(filepath)[0] + "/ver" + timestamp + os.path.splitext(filepath)[1],
 )
